39e24726b31fbfc4f16acff364590e9d4d005781
모로리 <gusdn6082@naver.com>
2021-08-18 01:52:26 +0900

    배치 관련 오류 수정

--- src/main/java/com/support/tool/batch/scheduler/CountingScheduler.java
+++ src/main/java/com/support/tool/batch/scheduler/CountingScheduler.java
@@ -19,7 +19,7 @@ public class CountingScheduler {
     private final Job job;
     private final JobLauncher jobLauncher;
 
-    @Scheduled(cron = "0 50 1 * * ?")
+    @Scheduled(cron = "0 0 2 * * ?")
     public void executeJob() {
         try {
             jobLauncher.run(
