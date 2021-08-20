76d1529db346ea1cf0c405d8969c11df6e596359
모로리 <gusdn6082@naver.com>
2021-08-20 15:12:01 +0900

    mail 수정

--- src/main/java/com/support/tool/aop/MatterAspect.java
+++ src/main/java/com/support/tool/aop/MatterAspect.java
@@ -8,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Aspect
 @Component
 @AllArgsConstructor
@@ -21,6 +23,7 @@ public class MatterAspect {
     public void sendEmailAfterRegisterMatter() {
         log.info("sendEmailAfterRegisterMatter called");
 
-        emailUtil.sendEmail("gusdn6082@gmail.com", "Support Tool: 새로운 안건이 등록되었습니다.", "내용 : ");
+        emailUtil.sendEmail("bizformlee@gmail.com", "Support Tool: 새로운 안건이 등록되었습니다.",
+                "일시 ( " + LocalDateTime.now() + " ) 새로운 안건이 등록되었으니 확인바랍니다.");
     }
 }
